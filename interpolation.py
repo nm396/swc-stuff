724b3d7c83fa516283e6465b17622821beaf940a
nm396 <nm396@st-andrews.ac.uk>
2013-12-13 09:55:14 +0000

    adding docstrings

--- interpolation.py
+++ interpolation.py
@@ -1,8 +1,13 @@
 class TimeSeries(object):
+    '''takes a parameter, data, of the form x,y tuples'''
+   
     def __init__(self, data):
+         '''this is to store the time_series data in self.data'''
         self.data = data
-    
+
+   
     def get(self, x):
+         '''if the variable is matching one of the data points, return the corresponding y value'''
         for (xi,yi) in self.data:
             if xi == x:
                 return yi
@@ -10,12 +15,14 @@ class TimeSeries(object):
         raise Exception("Didn't find the value")
     
     def view(self):
+        '''function to view the data, but empty function right now'''
         pass
 
 
     
 class StepFunctionTimeSeries(TimeSeries):
     def get(self, x):
+        '''if the new data point is closer to x than the last point, it becomes the new closest point'''
         closest_point = None
         for (xi, yi) in self.data:
             if closest_point is None:
@@ -30,10 +37,13 @@ class StepFunctionTimeSeries(TimeSeries):
     
 class LinearTimeSeries(TimeSeries):
     def __init__(self, data):
+        '''the data needs to be sorted. is called from a previous class to avoid mistakes''' 
         TimeSeries.__init__(self, data)
         self.data.sort()
     
     def get(self, x):
+         '''Method to find two closest data points to x and linearly
+    interpolate between them to estimate the y value'''
         # if it's out of range to the left,
         # return the first value
         if x < self.data[0][0]:
